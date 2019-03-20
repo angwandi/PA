7e58d20cf2138f2bcd72543f3875c9c59825905c
Innocent Angwadia <demsdema217@gmail.com>
2019-03-20 23:19:52 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello! My name is 2ms.");
+        System.out.println("I was created in 2019.");
+    }
+}
