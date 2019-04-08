9cf22f58c23fd796107585c0e7346a1731360ace
Innocent Angwadia <demsdema217@gmail.com>
2019-04-09 00:47:16 +0300

    Stage #5: Create classes

---
+++ src/com/company/GuessAge.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GuessAge {
+    static void guessAge() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+}
